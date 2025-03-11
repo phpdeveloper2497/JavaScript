56ef25dc8dd5657429aa37b3575a449f470ca15c
Rustam Ormonov <php2497@gmail.com>
2025-03-11 16:06:46 +0500

    JavaScript asoslari DOM gacha o'zlashtirildi

--- JS/New lessons/app.js
+++ JS/New lessons/app.js
@@ -1,4 +1,4 @@
-            // TYPE CONVERSION TURLARNI O'ZGARTIRISH
+// TYPE CONVERSION TURLARNI O'ZGARTIRISH
 
 // let result = '100';
 
@@ -10,7 +10,7 @@
 // console.log(typeof result);
 
 
-            //  PRACTICE
+//  PRACTICE
 
 // let ism = prompt('Hello, please enter your name:');
 // // console.log(ism);
@@ -20,7 +20,7 @@
 // let result = capitalLetter + letter
 // alert(`Hello, ${result} welcome to our site`)
 
-                // FOR LOOP
+// FOR LOOP
 
 // const names = ['Rustam', 'Sardor', 'Jamoliddin','Baxriddin', 'Bekzod'];
 
@@ -29,7 +29,7 @@
 // }
 
 
-                // WHILE LOOP
+// WHILE LOOP
 
 // const names = ['Rustam', 'Sardor', 'Jamoliddin','Baxriddin', 'Bekzod'];
 
@@ -39,7 +39,7 @@
 //     i++;
 // }
 
-                // DO WHILE LOOP 'shart bajaraishdan oldin bir marta ishlatib oladi keyin shartga qaraydi'
+// DO WHILE LOOP 'shart bajaraishdan oldin bir marta ishlatib oladi keyin shartga qaraydi'
 
 // const names = ['Rustam', 'Sardor', 'Jamoliddin','Baxriddin', 'Bekzod'];
 
@@ -58,12 +58,12 @@
 // } while(i < 5);
 
 
-              //     Continue and Break
+//     Continue and Break
 
 // let natijalar = [12,55,70,0,45,100,27,96];
 
 // for(let i = 0; i < natijalar.length; i++){
-   
+
 //     if(natijalar[i] == 0){
 //         continue;
 //    }
@@ -72,7 +72,7 @@
 //          break;
 //     }
 //     console.log("Sizning natijangiz:",natijalar[i]);
-  
+
 // }
 
 // let natijalar = [12,55,70,0,45,100,27,96];
@@ -92,10 +92,10 @@
 //     }
 // }
 
-                
-                            // SWITCH CASE
 
-    
+// SWITCH CASE
+
+
 // let baho = prompt("Qaysi baho haqida bilmoqchisiz").toLocaleUpperCase();
 
 // switch (baho) {
@@ -141,7 +141,7 @@
 // }
 
 
-                      ////// foreach bn
+////// foreach bn
 
 // const natijalar = [12, 55, 70, 0, 45, 100, 27, 96];
 // const ismlar = ['Anvar', 'Sobir', 'Akrom', 'Xurshid', 'Jasur', 'Bexruz', 'Dilshod', 'Doniyor'];
@@ -151,7 +151,7 @@
 // });
 
 
-                ///// BLOCK SCOPE //global va ichki e'lon qilingan o'zgaruvchilar
+///// BLOCK SCOPE //global va ichki e'lon qilingan o'zgaruvchilar
 
 // let age = 24
 
@@ -174,9 +174,9 @@
 
 
 
-                // // FUNCTIONS 
+// // FUNCTIONS 
 
-                /// DECLARATION FUNCTION
+/// DECLARATION FUNCTION
 
 // newFunction() // funksiyadan oldin ham uni nomini e'lon qilsa bo'ladi
 
@@ -209,10 +209,10 @@
 // byeByeBaby()
 
 
-                    // /// FUNCTION WITH RETURN
-    
-const circleSurface = function(r, pi = 3.14){
-    return pi* r **2 ;
+// /// FUNCTION WITH RETURN
+
+const circleSurface = function (r, pi = 3.14) {
+    return pi * r ** 2;
 }
 
 const s = circleSurface(10) // endi buni boshqa funksiyada chqirishimiz mumkin
@@ -222,8 +222,159 @@ const cylinder = (h) => {
     // return s*h;
 }
 
-cylinder(2) // console.log bo'lganda shunchaki e'lon qilinsa bo'ldi
+// cylinder(2) // console.log bo'lganda shunchaki e'lon qilinsa bo'ldi
 
-// const x = cylinder(2); // return qilinsa o'zgaruvchiga ob yana ishlatsa yoki console.log ga chiqarsa bo'ladi
+// // const x = cylinder(2); // return qilinsa o'zgaruvchiga ob yana ishlatsa yoki console.log ga chiqarsa bo'ladi
+// // console.log(x)
+
+
+////// ARROW FUNCTION QISQARTMASI
+
+// const hello = ism => `Hello ${ism}`
+
+// x = hello('Rustam')
+
+// console.log(hello('Rustam'))
 // console.log(x)
 
+/// ikkinchi usul
+
+// const hello = (ism,familiya) =>`Hello ${ism} ${familiya}`;
+
+// const call = hello('Rustam', 'Ormonov');
+// console.log(call)
+
+
+//                 /// EXAMPLE   tovarlar uchun soliq bilan narxini hisoblash
+
+// const bill = function(products, tax){
+//     let total = 0;
+
+//     for(let i = 0; i<products.length; i++){
+//         total+= products[i] + products[i] * tax;
+//     }
+
+//     return total;
+// }
+
+// const result = bill([23,45,76,58],0.2);
+
+// console.log(result)
+
+
+// // FUNCTION AND METHODS DIFFERANCE 
+
+// FUNCTION      // bitta o'zi alohida ishlatsa bo'ladi
+
+// const hello = () => {
+//     console.log("Hello world")
+// }
+// hello()    /// o'zi alohida ishlatilsa bo'ladi
+
+// METHODS      // doimo birorta data bilan birga ishlatiladi alohida ishlata olmaymiz
+
+// const ism = "Ahror";
+
+// const x = ism.toLocaleUpperCase();  /// biror dataga ishlatiladi
+// console.log(x)
+
+
+//     const pi = 3.14124141.toFixed(2)
+// console.log(pi)
+
+
+///  CALLBACK FUNCTION
+
+// const myFunc = (callBackFunc) =>{
+//     let number = 50;
+//     return callBackFunc(number)
+// }
+
+// myFunc((value) =>{
+//  console.log(value);
+// })
+
+
+////// FOREACH METHOD
+
+// const ismlar = ['sardor', 'rusTam','bekzod','baxRiddin','jamolidDin'];
+
+// console.log(ismlar);
+
+// const newIsmlar= [];
+
+// ismlar.forEach(function(ism){
+//     const newIsm = ism.charAt().toLocaleUpperCase() + ism.slice(1).toLocaleLowerCase() + "bek";
+//    newIsmlar.push(newIsm)
+// })
+
+//   console.log(newIsmlar)
+
+
+///// ////// ///// ///// / //// OBJECT //////// /   / / / /
+
+/////////        Objectga tashqaridan o'zgaruvchi biriktirish            ///////////
+
+// const films = [
+//     { name: 'Capitan america', likes: 300 },
+//     { name: 'Harry Poter', likes: 460 },
+//     { name: 'Troya', likes: 512 },
+//     { name: 'Tonny Star', likes: 623 },
+//     { name: 'Tom and Jerry', likes: 156 },
+// ];
+
+// const user = {
+//     name: 'Rustam',
+//     age: 28,
+//     email: 'php2497@gmail.com',
+//     addres: 'Toshkent city',
+//     profession: 'developer',
+//     langs: ['uzbek', 'russian'],
+//     login: function () {
+//         console.log("Siz profilga muvoffaqiyatli kirdingiz!")
+//     },
+//     logout: function () {
+//         console.log("Siz profildan chiqdingiz!")
+//     },
+//     speak: function () {
+//         console.log("I can speak:")
+//         this.langs.forEach(function (lang) {
+//             console.log(lang)
+//         })
+//     },
+//     moviWatch: function myFunc() {
+//         console.log(`${this.name} yoqtirgan filmlar nomi :`)
+
+//         films.forEach((film) => {
+//             const result = `Kino nomi: ${film.name} 🎥 , likes: ${film.likes} ❤️`
+//             console.log(result)
+
+//         })
+//     }
+// }
+
+// user.moviWatch()
+
+///// /////// //////////// ARRAY ICHIDA OBJECT YARATISH
+
+// const films = [
+//     { name: 'Capitan america', likes: 300 },
+//     { name: 'Harry Poter', likes: 460 },
+//     { name: 'Troya', likes: 512 },
+//     { name: 'Tonny Star', likes: 623 },
+//     { name: 'Tom and Jerry', likes: 156 },
+// ];
+
+// const newFilm = films.forEach((film) => {
+//     // console.log(film.name)
+//     // console.log(film.likes)
+//     const result = `Kino nomi: ${film.name} 🎥 , likes: ${film.likes} ❤️`
+
+//     console.log(result)
+// })
+
+
+
+
+
+///////////// AHROR SOLIYEV (AHROR WEB kanali ) ning 42 - darsigacha ya'ni DOM ga cha bajarildi ////////////
